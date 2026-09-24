5ab0259c0c6e0240e21e991079c6ab004f23dc08
Bigsk <i@ianxia.com>
2026-09-24 19:06:58 +0800

    fix(meta): update version to 3.2.1

--- v3/meta.go
+++ v3/meta.go
@@ -6,6 +6,6 @@ import (
 
 const Endpoint = "https://api.gh.ink/v3.1"
 
-const Version = "3.1.2"
+const Version = "3.2.1"
 const UserAgent = "GhinkOpenAPISDK-20260512-Go/" +
 	Version + " (" + runtime.GOOS + "; " + runtime.GOARCH + ")"
